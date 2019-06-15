ebd3454d6a5c5140a84c525b7ddeb612d7b70748
liuchialin <charles.ee96@g2.nctu.edu.tw>
2019-06-15 18:02:31 +0800

    first spring learning

---
+++ src/main/java/guru/springframework/spring5webApp/Spring5webAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5webApp/Spring5webAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
